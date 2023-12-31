2d45a376989efb372e17b2b6a6cb19d0a84fce52
TimofeyShved <timofeya123@inbox.ru>
2023-12-31 18:58:48 +0600

    login2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import java.security.PrivilegedAction;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
 	    // Подключаем самодельные правила политики безопастности
         System.setProperty("java.security.policy", "src/MyApp.policy");
+        System.setProperty("java.security.auth.login.config", "src/jaas.config");
 
         // включаем политику безопастности
         System.setSecurityManager(new SecurityManager());
@@ -12,7 +18,31 @@ public class Main {
         // и файле java отключить
         // permission java.util.PropertyPermission "os.name", "read"
 
-        // выводим название ОС (это можно заблокировать
+        // выводим название ОС (это можно заблокировать)
         System.out.println(System.getProperty("os.name"));
+
+        // так же можно авторизоваться через настройки, выдать себе права
+        LoginContext loginContext = new LoginContext("Login1");
+        loginContext.login();
+        Subject subject = loginContext.getSubject();
+        System.out.println("Авторизован");
+
+        // И выполнить нужный нам код
+        Subject.doAsPrivileged(
+                subject,
+                new PrivilegedAction<Object>() {
+                    @Override
+                    public Object run() {
+
+                        // Например этот код
+                        System.out.println(System.getProperty("os.name"));
+
+                        return null;
+                    }
+                },
+                null);
+
+        loginContext.logout();
+
     }
 }
